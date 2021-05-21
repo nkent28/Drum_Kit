95fe4a3f88b8c7fffaff6c3f906a403c3b3175d3
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2021-05-21 15:35:09 +0100

    Added this method to JS code

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0 ; i < numberOfDrumButtons; i++) {
    document.querySelectorAll(".drum")[i].addEventListener('click' , function() {
-    alert("I got clicked!");
+      this.style.color = "white";
   });
 }
+
+  // let audio = new Audio('sounds/tom-1.mp3');
+  // audio.play();
